d982640ce3f010b3b9816a4ae467a692a3976a51
Juan David Cuervo <juan.cuervo@vendemas.com.pe>
2021-09-05 19:48:31 -0500

    Final Proyect

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-app.use(express.static('./public')); 
 
-app.listen('3000', ()=> console.log('Server listening in http:/localhost:3000'))
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+
+app.use(express.static('./public'));
+
+app.listen('3000', () => console.log('Server listening in http:/localhost:3000'));
